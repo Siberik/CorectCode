300342de521b06b405e0e63e8a418a2bd0164fd8
user25 <user25@user25.ru>
2022-11-22 16:47:15 +0500

    Незаконченная версия программы

--- CorectCode/CorrectCodeClass.cs
+++ CorectCode/CorrectCodeClass.cs
@@ -20,33 +20,40 @@ namespace CorectCodeLibrary
         public static bool IsCorrectCode(string candidate)
         {
             var candidat = candidate.Where(x=>char.IsDigit(x)).ToArray();
-            
-            //if (candidat.Length<10)
-            //{
-            //    throw new Exception("Вы ввели недостаточно символов");
-            //}
-           for(int i=0;i>8;i++)
+
+            if (candidat.Length < 10 || candidat.Any(x=>char.IsLetter(x))||candidat.Length>=11)
             {
-                int count = Convert.ToInt32(candidat[i]);
+                return false;
+            }
+            for (int i = 0; i < candidat.Length; i++)
+            {
+                int count=0;
+                while(i==9)
+                {
+                 count = Convert.ToInt32(candidat[i]);
+                }
+                
                 int last = Convert.ToInt32(candidat[9]);
                 int count2 = 0;
-                count2 +=count;
-                if(count2%10==0||last==0)
-                {
-                    return true;
-                }
-                if (count2 % 3 == 0 || last == 1)
-                {
-                    return true;
-                }
-                if(last==9)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                count2 += count;
+                
+                    if (count2 % 10 == 0 && last == 0)
+                    {
+                        return true;
+                    }
+                    if (count2 % 3 == 0 && last == 1)
+                    {
+                        return true;
+                    }
+                    if (last == 9)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                
             }
             return false;
         }
