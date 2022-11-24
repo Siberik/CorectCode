70c16430d11778edc9310b9de742ada4d4f624a3
user25 <user25@user25.ru>
2022-11-24 14:17:04 +0500

    Сделаны тесты

--- CorectCode/CorrectCodeClass.cs
+++ CorectCode/CorrectCodeClass.cs
@@ -19,20 +19,20 @@ namespace CorectCodeLibrary
         /// </returns>
         public static bool IsCorrectCode(string candidate)
         {
-            var candidat = candidate.Where(x=>char.IsDigit(x)).ToArray();
+            var candidat = candidate.Where(x => char.IsDigit(x)).ToArray();
 
-            if (candidat.Length < 10 || candidat.Any(x=>char.IsLetter(x))||candidat.Length>=11)
+            if (candidat.Length < 10 || candidat.Any(x => char.IsLetter(x)) || candidat.Length >= 11)
             {
                 return false;
             }
             int count2 = 0;
             int count = 0;
             int i = 0;
-            while (i <= 9)
+            while (i < 9)
             {
                 count = Convert.ToInt32(Char.GetNumericValue(candidat[i]));
 
-              
+
 
                 count2 += count;
                 i++;
@@ -57,7 +57,7 @@ namespace CorectCodeLibrary
                     }
                 
             }
-            return false;
+            
         }
     }
-}
+
--- CorrectCodeLibraryTest/CorrectCodeClassTests.cs
+++ CorrectCodeLibraryTest/CorrectCodeClassTests.cs
@@ -60,13 +60,65 @@ namespace CorrectCodeLibraryTest
             Assert.IsFalse(actual);
         }
         /// <summary>
-        /// Проверка на ввод некорректных символов
+        /// Проверка на ввод корректного числа
         /// </summary>
         [TestMethod]
         public void IsCorrectCode_IntDivide10_ReturnedTrue()
         {
             //Arrange
             string candidate = "1234123400";
+            //Act
+            bool actual = CorrectCodeClass.IsCorrectCode(candidate);
+            //Assert
+            Assert.IsTrue(actual);
+        }
+        /// <summary>
+        /// Проверка на ввод некоректоного последнего числа
+        /// </summary>
+        [TestMethod]
+        public void IsCorrectCode_IntNotDivide10_ReturnedFalse()
+        {
+            //Arrange
+            string candidate = "1234123401";
+            //Act
+            bool actual = CorrectCodeClass.IsCorrectCode(candidate);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка на ввод некорректных значении
+        /// </summary>
+        [TestMethod]
+        public void IsCorrectCode_IntNotDivide_ReturnedFalse()
+        {
+            //Arrange
+            string candidate = "1234123404";
+            //Act
+            bool actual = CorrectCodeClass.IsCorrectCode(candidate);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка на ввод значении делимых на 3
+        /// </summary>
+        [TestMethod]
+        public void IsCorrectCode_IntDivide3_ReturnedTrue()
+        {
+            //Arrange
+            string candidate = "3000030001";
+            //Act
+            bool actual = CorrectCodeClass.IsCorrectCode(candidate);
+            //Assert
+            Assert.IsTrue(actual);
+        }
+        /// <summary>
+        /// Проверка на ввод ряда оканчивающее на 9
+        /// </summary>
+        [TestMethod]
+        public void IsCorrectCode_IntDivide_ReturnedTrue()
+        {
+            //Arrange
+            string candidate = "3000130009";
             //Act
             bool actual = CorrectCodeClass.IsCorrectCode(candidate);
             //Assert
